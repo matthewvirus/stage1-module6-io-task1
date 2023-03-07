dc314864b384bbaa796301b8cbeea582a4a7e7f6
Matthew Zhigalo <matthewzhigalo@gmail.com>
2023-03-07 17:03:16 +0300

    Fixed

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,14 +1,26 @@
 package com.epam.mjc.io;
 
+import org.apache.logging.log4j.core.impl.Log4jLogEvent;
+
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 public class FileReader {
 
+    Logger log = LogManager.getLogManager().getLogger(Log4jLogEvent.class.getName());
+
     public Profile getDataFromFile(File file) {
-        HashMap<String, String> profileMap = extractMapFromFile(file);
+        HashMap<String, String> profileMap = null;
+        try {
+            profileMap = extractMapFromFile(file);
+        } catch (FileNotFoundException e) {
+            log.info(e.getMessage());
+        }
         return new Profile(
                 profileMap.get("Name"),
                 Integer.parseInt(profileMap.get("Age")),
@@ -17,7 +29,7 @@ public class FileReader {
         );
     }
 
-    private HashMap<String, String> extractMapFromFile(File file) {
+    private HashMap<String, String> extractMapFromFile(File file) throws FileNotFoundException {
         HashMap<String, String> map = new HashMap<>();
         try (BufferedReader fileReader = new BufferedReader(new java.io.FileReader(file))) {
             String line;
@@ -28,17 +40,14 @@ public class FileReader {
                     String value = keyValuePair[1];
                     map.put(key, value);
                 } else {
-                    System.out.println("No Key:Value found in line, ignoring: " + line);
+                    log.info("No Key: Value found in line, ignoring: " + line);
                 }
             }
+        } catch (FileNotFoundException e) {
+            throw new FileNotFoundException(e.getMessage());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
         return map;
-    }
-
-    public static void main(String[] args) {
-        Profile profile = new FileReader().getDataFromFile(new File("src/main/resources/Profile.txt"));
-        System.out.println(profile.toString());
     }
 }
